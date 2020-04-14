10c0566fc05aa34fedc1cd51f2405c8d475b05d9
skuley <skuleyandpotao@gmail.com>
2020-04-14 22:37:51 +0900

    오후10시37분

--- a003_listener/src/main/java/com/lec/android/a004_widget/Main2Activity.java
+++ a003_listener/src/main/java/com/lec/android/a004_widget/Main2Activity.java
@@ -3,8 +3,11 @@ package com.lec.android.a004_widget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button num0, num1, num2, num
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
